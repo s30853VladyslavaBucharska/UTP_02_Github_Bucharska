cb00ab292056fbd0f323b78ea6e8b7fb2d853aee
Mari Kibets <mariakibec7@Gmail.com>
2024-10-09 11:57:52 +0200

    Change the 7th

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         Adder adder = new Adder();
         System.out.println(adder.add(1, 2));
         Subtractor subtractor = new Subtractor();
-        System.out.println(subtractor.subtract(6, 3));
+        System.out.println(subtractor.subtract(800, 90));
     }
 }
