a20f2208be03e128ca0caad1f87154e17638d4d5
Vladyslava <buchasrka.vladyslava@gmail.com>
2024-10-09 11:29:25 +0200

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,4 @@
-class Adder {
-    public int add(int a, int b) {
-        return a + b;
-    }
-}
-
-class Subtractor {
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-}
+// OK, I will add ‘Adder‘ and s30855 will add ‘Subtractor‘.
 
 public class Main {
     public static void main(String[] args) {
