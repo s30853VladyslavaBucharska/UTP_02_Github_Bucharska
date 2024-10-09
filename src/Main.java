32a8b4862b2853774002eaae158b012782b5b15e
Vladyslava <buchasrka.vladyslava@gmail.com>
2024-10-09 11:57:27 +0200

    Additional commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
-        System.out.println(adder.add(1, 2));
+        System.out.println(adder.add(100, 100));
         Subtractor subtractor = new Subtractor();
         System.out.println(subtractor.subtract(6, 3));
     }
